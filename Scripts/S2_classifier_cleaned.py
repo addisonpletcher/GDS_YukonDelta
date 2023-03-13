c9a4cea4a08d4c9ec04ad426f41d60612255e678
addisonpletcher <115518722+addisonpletcher@users.noreply.github.com>
2023-03-13 12:19:32 -0700

    Cleaned script for use by others

--- Scripts/S2_classifier_cleaned.py
+++ Scripts/S2_classifier_cleaned.py
@@ -11,28 +11,26 @@ from rasterio.plot import show
 import numpy as np
 import os
 import geopandas as gpd
-
-os.getcwd()
-# To go back one folder in cwd
-os.chdir("..")
-
-#%% Open summer Sentinel-2 image for training
-src = rasterio.open("QGIS\S2_tile.tif")
-show(src)
-
-#Open classification shapefiles
-land = gpd.read_file("QGIS\Land.shp")
-water = gpd.read_file("QGIS\Water.shp")
-print(land)
-print(water)
-
-
-#%% prep to sample Sentinel-2 
 from rasterio import mask as msk
 from shapely.geometry import mapping
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import confusion_matrix
 
+#%% Open summer Sentinel-2 image for training
+src = rasterio.open('...')
+show(src)
+
+#Open classification shapefiles
+land = gpd.read_file('...')
+water = gpd.read_file('...')
+print(land)
+print(water)
+
+#%% prep to sample Sentinel-2 
 land_proj = land.to_crs('EPSG:32603')
 #land_proj.crs
 
@@ -46,13 +44,8 @@ sa_array_land, clipped_transform = msk.mask(src, [mapping(geom) for geom in land
 
 all_land = []
 for b in range(src.count): 
-    #Drop zeros, mask to make one dimensional list (all bands)
     temp_list_L=sa_array_land[b][np.nonzero(sa_array_land[b])]
     all_land.append(temp_list_L)
-
-#Check length, should be amount of bands we have
-#len(all_land)
-
 # Convert to df
 land_df = pd.DataFrame(all_land).T
 
@@ -65,32 +58,22 @@ for c in range(src.count):
     #Drop zeros, mask to make one dimensional list (all bands)
     temp_list_W = sa_array_water[c][np.nonzero(sa_array_water[c])]
     all_water.append(temp_list_W)
-    
-#Check length, should be amount of bands we have
-#len(all_water)
-
 # Convert to df
 water_df = pd.DataFrame(all_water).T
 
-# No longer needed because included in for loop
-#sa_array_water[0][np.nonzero(sa_array_water[0])]
-
 #%% Combine dataframes, add column 
-
 land_df['label'] = 1
 water_df['label'] = 2
 
 final_df = pd.concat([land_df,water_df],ignore_index=True)
 
-#Rename Columns (Extra column?? No! band 8A is the last one, need to reconfig for this)
+#Rename Columns
 final_df.rename(columns = {0:'Band 1', 1:'Band 2', 2:'Band 3', 3:'Band 4', 4:'Band 5', 5:'Band 6', 6:'Band 7', 7:'Band 8', 8:'Band 8A', 9:'Band 9', 10:'Band 10', 11:'Band 11', 12:'Band 12'}, inplace = True)
 final_df
 
 #%% Train Machine Learning Model
-from sklearn.preprocessing import StandardScaler
-
-#Define Feature List (Green, Red, NIR)
-feature_list = ['Band 3', 'Band 4', 'Band 8']
+#Define Feature List
+feature_list = ['...', '...', '...']
 
 #Define features/labels
 X = final_df[feature_list]
@@ -103,14 +86,9 @@ df_scaled = pd.DataFrame(X_scaled, columns=feature_list)
 print(df_scaled)
 
 #Split data into training/testing subsets
-from sklearn.model_selection import train_test_split
-
-# Split data 
 X_train, X_test, y_train, y_test = train_test_split(df_scaled, y, test_size=0.2, random_state=42)
 
 #%% RandomForests model
-from sklearn.ensemble import RandomForestRegressor
-
 forest_reg = RandomForestRegressor(n_estimators = 30) #Define
 forest_reg.fit(X_train, y_train) #Fit
 
@@ -118,11 +96,10 @@ forest_reg.fit(X_train, y_train) #Fit
 predictions = forest_reg.predict(X_test)
 
 #%% Compute Confusion Matrix
-from sklearn.metrics import confusion_matrix
 print(confusion_matrix(y_test, predictions.astype(int)))
 
 #%% Import Spring S2 imagery
-spring = rasterio.open("S2_spring21.tif")
+spring = rasterio.open('...')
 
 # Read, change to 3D array
 band_list = []
