07e07be1a17806c689200a1810ae180d00e3e083
kbonnville <122561595+kbonnville@users.noreply.github.com>
2023-03-15 21:57:36 -0700

    Update and rename CreateFeatureMatrix_SARimage.py to CreateFeatureMatrix.py
    Cleaned up code/ added comments

--- CreateFeatureMatrix.py
+++ CreateFeatureMatrix.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-This script is adapted from the RemoteSensing-of-River-Ice tutorial (https://github.com/SdeRodaHusman/remotesensing-of-river-ice/blob/main/README.md) by SdeRodaHusman (contact: S.deRodaHusman@tudelft.nl).
+This script is adapted from the RemoteSensing-of-River-Ice tutorial (https://github.com/SdeRodaHusman/remotesensing-of-river-ice/blob/main/README.md).
 This script is used to create a csv-file with all feature information of the preprocessed SAR
 images included. Each row in the matrix describes a pixel in the SAR image with all feature values 
 that are computed (e.g. intensity, polarimetric and texture features.)
@@ -12,7 +12,6 @@ a preprocced feature of interest. This is the output file of: Scripts/XMLgraphs/
 PreprocessedImages.xml
 In this example, the following features were used:
     - Intenisty features: VV, VH
-    - Polarimetric decomposition parameters: Alpha, Anisotropy, Entropy
     - Texture features: GLCM mean, GLCM variance, GLCM correlation
     
 @author: Kelly Bonnville-Sexton , created on  March 2, 2023
@@ -26,14 +25,18 @@ from netCDF4 import Dataset
 import numpy as np
 import pandas as pd
 import numpy.ma as ma
-os.getcwd()  #not needed in final code
 #%% 
-data = ... #(give the location of the NetCDF file, which is an output product of Scripts/XMLgraphs/Combine
-#PreprocessedImages.xml)
-output_path = os.path.abspath(...)  #(Give your output path to save the feature matrix of the prepocessed SAR image including all features of interest)
+
+#give the locations of the NetCDF files with preprocessed images
+data1 =  ...
+data2 =  ...
+
+#(Give your output path to save the feature matrix of the prepocessed SAR image including all features of interest)
+output_path = os.path.abspath("...")  
 
 # Read the NetCDF file
-read_nc = Dataset(data, 'r')
+read_nc = Dataset(data2, 'r')
+read_nc2 = Dataset(data1, 'r')
 
 #%%
 # Create array of lon and lat coordinates of each pixel
@@ -47,7 +50,7 @@ XX_lon,YY_lat= np.meshgrid(lon_unmasked,lat_unmasked,sparse=False)
 
 #%%
 
-# Reformat VV ;     
+# Reformat VV ;
 VV = read_nc.variables['Gamma0_VV_db'][:]
 VV_unmasked = ma.getdata(VV)
 VV_unmasked[VV_unmasked == 0] = 'nan'
@@ -73,7 +76,7 @@ lat_final = lat_unravel[lat_unravel != -9999]
 
 #%%
 # Reformat VH    
-VH = read_nc.variables['Gamma0_VV_db'][:]
+VH = read_nc.variables['Gamma0_VH_db'][:]
 VH_unmasked = ma.getdata(VH)
 VH_unravel = np.ravel(VH_unmasked)
 (VH_unravel[location_nans])=-9999
@@ -84,51 +87,21 @@ VH_final = VH_unravel[VH_unravel != -9999]
 depol_ratio = (VH_final / VV_final)
 
 #%%
-#Dont need due if using dB bands
-'''
-VV_log = 10*np.log10(VV_final)
-VH_log = 10*np.log10(VH_final)
-'''
-
-#%%
-
-# Reformat polarimetric features
-alpha = read_nc.variables['Alpha_S0'][:]
-alpha_unmasked = ma.getdata(alpha)
-alpha_unravel = np.ravel(alpha_unmasked)
-(alpha_unravel[location_nans])=-9999
-alpha_final = alpha_unravel[alpha_unravel != -9999]
-
-anis = read_nc.variables['Anisotropy_S0'][:]
-anis_unmasked = ma.getdata(anis)
-anis_unravel = np.ravel(anis_unmasked)
-(anis_unravel[location_nans])=-9999
-anis_final = anis_unravel[anis_unravel != -9999]
-
-entr = read_nc.variables['Entropy_S0'][:]
-entr_unmasked = ma.getdata(entr)
-entr_unravel = np.ravel(entr_unmasked)
-(entr_unravel[location_nans])=-9999
-entr_final = entr_unravel[entr_unravel != -9999]
-
-
-#%%
 
 # Reformat texture features
-GLCMmean = read_nc.variables['texture_GLCMMean_S1'][:]
+GLCMmean = read_nc2.variables['Gamma0_VH_db_GLCMMean'][:]
 GLCMmean_unmasked = ma.getdata(GLCMmean)
 GLCMmean_unravel = np.ravel(GLCMmean_unmasked)
 (GLCMmean_unravel[location_nans])=-9999
 GLCMmean_final = GLCMmean_unravel[GLCMmean_unravel != -9999]
-GLCMmean_final
 
-GLCMvariance = read_nc.variables['norm_GLCMVariance_S1'][:]
+GLCMvariance = read_nc2.variables['Gamma0_VH_db_GLCMVariance'][:]
 GLCMvariance_unmasked = ma.getdata(GLCMvariance)
 GLCMvariance_unravel = np.ravel(GLCMvariance_unmasked)
 (GLCMvariance_unravel[location_nans])=-9999
 GLCMvariance_final = GLCMvariance_unravel[GLCMvariance_unravel != -9999]
 
-GLCMcorrelation = read_nc.variables['norm_GLCMCorrelation_S1'][:]
+GLCMcorrelation = read_nc2.variables['Gamma0_VH_db_GLCMCorrelation'][:]
 GLCMcorrelation_unmasked = ma.getdata(GLCMcorrelation)
 GLCMcorrelation_unravel = np.ravel(GLCMcorrelation_unmasked)
 (GLCMcorrelation_unravel[location_nans])=-9999
@@ -136,7 +109,15 @@ GLCMcorrelation_final = GLCMcorrelation_unravel[GLCMcorrelation_unravel != -9999
 
 
 #%%
-# Write data to CSV file
-df_final = pd.DataFrame({'lon': lon_final, 'lat':lat_final, 'VV': VV_log, 'VH': VH_log, 'Depol_ratio':depol_ratio#, 'Alpha': alpha_final, 'Anisotropy': anis_final, 'Entropy': entr_final, 'GLCMMean': GLCMmean_final, 'GLCMVariance': GLCMvariance_final, 'GLCMCorrelation': GLCMcorrelation_final})
+#Write data to CSV file
+df_final = pd.DataFrame({'lon': lon_final, 'lat':lat_final, 'VV_instensity': VV_final, 
+                         'VH_intensity': VH_final, 'GLCM_mean': GLCMmean_final})
+df_final.to_csv(output_path +'\FeatureMatrix_subset.csv', index = False, header=True)
+
+#%%
+# Write data to CSV file 
+df_final = pd.DataFrame({'lon': lon_final, 'lat':lat_final, 'VV_instensity': VV_final, 
+                         'VH_intensity': VH_final, 'Depol_ratio':depol_ratio, 'GLCM_mean': GLCMmean_final, 'GLCM_variance': GLCMvariance_final, 'GLCM_correlation': GLCMcorrelation_final})
                          
-df_final.to_csv(output_path +'\FeatureMatrix_SARimage.csv', index = False, header=True)   
+my_file = 'FeatureMatrix_SARimage.csv'
+df_final.to_csv(output_path, my_file, index = False, header=True)  
